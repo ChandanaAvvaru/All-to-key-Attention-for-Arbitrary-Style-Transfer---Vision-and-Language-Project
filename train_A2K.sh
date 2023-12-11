8e294ca50d6e5d5421c46754bf352654dfa1e18a
saichandana2410 <saichandanaavvaru@gmail.com>
2023-12-11 17:05:18 -0500

    Final Submission Vision and Laguage

---
+++ train_A2K.sh
@@ -0,0 +1,21 @@
+
+python /content/drive/MyDrive/StyA2K/train.py \
+--content_path /content/train2014 \
+--style_path /content/wikiart \
+--name A2K \
+--model A2K \
+--dataset_mode unaligned \
+--no_dropout \
+--load_size 512 \
+--crop_size 256 \
+--image_encoder_path /content/drive/MyDrive/StyA2K/checkpoints/vgg_normalised.pth \
+--gpu_ids 0 \
+--batch_size 8 \
+--n_epochs 2 \
+--n_epochs_decay 3 \
+--display_freq 1 \
+--display_port 8097 \
+--display_env A2K \
+--lambda_no_param_A2K 1.25 \
+--lambda_global 10 \
+--lambda_content 0 
